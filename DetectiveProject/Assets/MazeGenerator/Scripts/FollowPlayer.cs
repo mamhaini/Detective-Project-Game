39c256445cb7c65c31874236f85d00c854fe6a2d
BerkanHRGL <329918@student.mboutrecht.nl>
2021-02-17 14:22:30 +0100

    Movement and Camera added
    Camera follows Player

---
+++ DetectiveProject/Assets/MazeGenerator/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 7, -13);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Offset the camera behind the player 
+        transform.position = player.transform.position + offset;
+    }
+}
