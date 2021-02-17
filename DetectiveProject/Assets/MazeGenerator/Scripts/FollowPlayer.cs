e04b918be82439b436aa583f28137aea4f2ab581
InfernoMa <infernoma@hotmail.com>
2021-02-17 15:32:29 +0100

    Added Sounds and pickup
    Sounds gets played when a coin gets picked up

--- DetectiveProject/Assets/MazeGenerator/Scripts/FollowPlayer.cs
+++ DetectiveProject/Assets/MazeGenerator/Scripts/FollowPlayer.cs
@@ -5,19 +5,60 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 7, -13);
+   
+    
+    public GameObject ViewCamera = null;
+    public AudioClip CoinSound = null;
+
+    private Rigidbody mRigidBody = null;
+    private AudioSource mAudioSource = null;
+
+    // public GameObject player;
+    // private Vector3 offset = new Vector3(0, 7, -13);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        mRigidBody = GetComponent<Rigidbody>();
+        mAudioSource = GetComponent<AudioSource>();
+    }
+    //Camera Follows Player 
+    void FixedUpdate()
+    {
+        if (ViewCamera != null)
+        {
+            Vector3 direction = (Vector3.up * 2 + Vector3.back) * 2;
+            RaycastHit hit;
+            Debug.DrawLine(transform.position, transform.position + direction, Color.red);
+            if (Physics.Linecast(transform.position, transform.position + direction, out hit))
+            {
+                ViewCamera.transform.position = hit.point;
+            }
+            else
+            {
+                ViewCamera.transform.position = transform.position + direction;
+            }
+            ViewCamera.transform.LookAt(transform.position);
+        }
+    }
+    // Sound gets played when player picks up a coin
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Coin"))
+        {
+            if (mAudioSource != null && CoinSound != null)
+            {
+                mAudioSource.PlayOneShot(CoinSound);
+            }
+            Destroy(other.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // Offset the camera behind the player 
-        transform.position = player.transform.position + offset;
+        
+        // transform.position = player.transform.position + offset;
     }
 }
