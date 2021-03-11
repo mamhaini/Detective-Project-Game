e13a04e5e36940f3e5b469d66162f0215d4c0d05
InfernoMa <infernoma@hotmail.com>
2021-03-11 15:40:54 +0100

    Player Animations added and remove menu text fixed

---
+++ DetectiveProject/Assets/MazeGenerator/Scripts/CanvasText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasText : MonoBehaviour
+{
+    // Canvas that appears when the game has not started 
+    public Canvas canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Canvas gets dissabled upon play
+        canvas = canvas.GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- DetectiveProject/Assets/MazeGenerator/Scripts/PlayerMovement.cs
+++ DetectiveProject/Assets/MazeGenerator/Scripts/PlayerMovement.cs
@@ -5,32 +5,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator anim;
+    public float playerSpeed = 5.0f;
+
     private CharacterController controller;
     private Vector3 playerVelocity;
-    public float playerSpeed = 5.0f;
+   
 
 
     private void Start()
     {
+        // Character Controller
         controller = gameObject.AddComponent<CharacterController>();
         controller.height = 0.1f;
         controller.radius = 0.1f;
+        // Animator reference
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        
+        
       
-        if ( playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
+        // Movement
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
+        // Movement and run animation condition
         if (move != Vector3.zero)
         {
+            anim.SetInteger("Condition", 1);
             gameObject.transform.forward = move;
+        }
+        else
+        {
+            anim.SetInteger("Condition", 0);
         }
 
 
