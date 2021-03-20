9f5845435c9ea39a76464b7c0824b0275a20b698
BerkanHRGL <329918@student.mboutrecht.nl>
2021-03-20 20:26:27 +0100

    Ending scene and Score counter added

---
+++ DetectiveProject/Assets/MazeGenerator/Scripts/CollectCoin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectCoin : MonoBehaviour
+{
+    public int levelComplete = 10;
+    void OnTriggerEnter(Collider other)
+    {
+        ScoringSystem.theScore += 1;
+        Destroy(gameObject);
+
+        if (ScoringSystem.theScore == levelComplete)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
